523fc56ca236499440862ab9174fb4be75eff99e
Johannes <johannesjandke@gmail.com>
2021-04-17 21:59:30 +0200

    Basic structure is present. Output must still be processed and brought into a pretty form.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("raflu Reliable and fast Linux updater")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+
+def update_check():
+    update = subprocess.Popen(["apt-get --just-print upgrade"], shell=True, stdout=subprocess.PIPE)
+    out = update.stdout.read()
+    send_message(out)
+
+
+def send_message(out):
+    mTitle = "New updates are available"
+    mText = "Following updates are available:\n {updates}".format(updates = out)
+    os.system('notify-send "' + mTitle + '" "' + mText + '"')
+    print("\n\n", out)
+
+
+update_check()
