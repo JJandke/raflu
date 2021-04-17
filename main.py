96efe97ca1604ef724d50f1121d02424537323ce
Johannes <johannesjandke@gmail.com>
2021-04-17 23:02:16 +0200

    One-digit numbers of available updates can now be read out (count). However, it must still be possible to process two- and three-digit numbers, as well as to list the package names.

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ import subprocess
 
 def update_check():
     update = subprocess.Popen(["apt-get --just-print upgrade"], shell=True, stdout=subprocess.PIPE)
-    out = update.stdout.read()
-    send_message(out)
+    out = "%s" % (update.stdout.read())
+    format_output(out)
+
+
+def format_output(out):
+    a = out.find("und")
+    out_number = a +4
+    count = out[out_number]
+    print(count)
 
 
 def send_message(out):
     mTitle = "New updates are available"
-    mText = "Following updates are available:\n {updates}".format(updates = out)
+    mText = "Following updates are available:\n {updates}".format(updates=out)
     os.system('notify-send "' + mTitle + '" "' + mText + '"')
     print("\n\n", out)
 
